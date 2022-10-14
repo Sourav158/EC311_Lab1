4114f718db0c26c09a54cd9ec4c19a35f2daa57a
Sourav158 <114108771+Sourav158@users.noreply.github.com>
2022-10-14 19:57:25 -0400

    Create leftshift_testbench.v

---
+++ alu_4_bit/tb/leftshift_testbench.v
@@ -0,0 +1,19 @@
+module leftshift_testbench(
+
+    );
+    
+    reg [3:0] a_i, b_i;
+    wire [7:0] c_o;
+    
+    left_shift L1(.a_i(a_i), .b_i(b_i), .c_o(c_o));
+    
+    initial begin
+        a_i = 0; b_i = 0;
+    end
+    
+    always begin
+        #2 {a_i, b_i} = {a_i, b_i} + 1'b1;
+        if ((a_i == 4'b1111) && (b_i == 4'b1111)) #10 $finish;
+    end
+    
+endmodule
