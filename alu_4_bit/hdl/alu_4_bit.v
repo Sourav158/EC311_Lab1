fc4625e99f47a14e471a80276e3bc1b3fa75ee90
Sourav158 <114108771+Sourav158@users.noreply.github.com>
2022-10-14 19:36:52 -0400

    Update alu_4_bit.v

--- alu_4_bit/hdl/alu_4_bit.v
+++ alu_4_bit/hdl/alu_4_bit.v
@@ -1,25 +1,40 @@
 `timescale 1ns / 1ps
-
-// EC-311 Lab-1 Part-2
-  // The names of the variables are as described in the lab handout
-
-module alu_4_bit #
-(
-  parameter INPUT_WIDTH = 4,
-  parameter OUTPUT_WIDTH = 8,
-  parameter SELECT_WIDTH = 2
-)
-(
-  // The inputs 
-  input wire [INPUT_WIDTH-1:0]      A_i,
-  input wire [INPUT_WIDTH-1:0]      B_i,
-  input wire [SELECT_WIDTH-1:0]     S_i,
-
-  // The outputs
-  output wire [OUTPUT_WIDTH-1:0]           Y_o
-
-);
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2022 03:05:41 AM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 
+module alu(
+    input [3:0]a_i,
+    input [3:0]b_i,
+    input [1:0]s_i,
+    output [7:0]c_o
+    );
+    
+    wire [7:0] w0, w1, w2, w3, w4;
+    
+    four_bit_adder FBA1(.a_i(a_i), .b_i(b_i), .c_o(w0));
+    multiplier M1(.a_i(a_i), .b_i(b_i), .c_o(w1));
+    left_shift L1(.a_i(a_i), .b_i(b_i), .c_o(w2));
+    concatenator C1(.a_i(a_i), .b_i(b_i), .c_o(w3));
+    
+    Mux Mx1(.j_i(w0), .k_i(w2), .l_i(w1), .m_i(w3), .s_i(s_i), .c_o(w4));
+    
+    assign c_o = w4;
 endmodule
